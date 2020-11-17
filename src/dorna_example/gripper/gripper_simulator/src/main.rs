cb966bcfa0cd81c6127d265d0aa0a192df29711b
Martin Dahl <martin.dahl@gmail.com>
2020-11-17 15:11:00 +0100

    use simulator data in gripper simulator

--- src/dorna_example/gripper/gripper_simulator/src/main.rs
+++ src/dorna_example/gripper/gripper_simulator/src/main.rs
@@ -2,19 +2,18 @@ use r2r;
 use failure::Error;
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::time::Duration;
 
 use r2r::gripper_msgs::msg::{Goal, State};
+use r2r::std_msgs::msg::String;
 
-use rand::Rng;
+use serde_json;
 
 mod resource_handler;
 
 #[derive(PartialEq, Copy, Clone)]
 enum GripperState {
     Open,
-    ClosedWithPart,
-    ClosedWithoutPart
+    Closed,
 }
 
 fn main() -> Result<(), Error> {
@@ -24,40 +23,28 @@ fn main() -> Result<(), Error> {
     let state_publisher = node.create_publisher::<State>("state")?;
 
     let state = Rc::new(RefCell::new(GripperState::Open));
+    let sim_state = Rc::new(RefCell::new(false));
     let namespace = node.namespace().unwrap();
     let initial_goal = Goal{close: false};
     let sp_comm = crate::resource_handler::ResourceHandler::new(
-        &mut node, 
-        namespace, 
+        &mut node,
+        namespace,
         serde_json::to_string(&initial_goal).unwrap()
     ).unwrap();
 
     // goal callback
     let state_cb = state.to_owned();
     let nl_cb = node.logger().to_owned();
-
     let sp_goal_cb = move |msg: Goal| {
         let mut state = state_cb.borrow_mut();
         sp_comm.last_goal(serde_json::to_string(&msg).unwrap());
 
         if msg.close && *state == GripperState::Open {
             r2r::log_info!(&nl_cb, "closing the gripper");
-
-            // take transition slowly so we see what's going on
-            std::thread::sleep(Duration::from_millis(2000));
-            let fail = rand::thread_rng().gen_range(0,5) < 1;
-            if fail {
-                r2r::log_warn!(&nl_cb, "closed without a part");
-                *state = GripperState::ClosedWithoutPart;
-            } else {
-                r2r::log_info!(&nl_cb, "closed with a part");
-                *state = GripperState::ClosedWithPart;
-            }
+            *state = GripperState::Closed;
         }
-        else if !msg.close && (*state != GripperState::Open) {
+        else if !msg.close && (*state == GripperState::Closed) {
             r2r::log_info!(&nl_cb, "opening the gripper");
-            std::thread::sleep(Duration::from_millis(2000));
-            r2r::log_info!(&nl_cb, "opened the gripper");
             *state = GripperState::Open;
         }
     };
@@ -65,14 +52,29 @@ fn main() -> Result<(), Error> {
     let _goal_sub = node.subscribe("goal", Box::new(sp_goal_cb))?;
 
 
+    let sim_state_cb = sim_state.to_owned();
+    let sim_cb = move |msg: String| {
+        let str = msg.data.clone();
+        let cubes = serde_json::from_str::<Vec<serde_json::Value>>(&msg.data);
+        if let Ok(cubes) = cubes.as_ref() {
+            *sim_state_cb.borrow_mut() = false;
+            for c in cubes {
+                if c.get("position") == Some(&serde_json::Value::String("/dorna/r1/dorna_5_link".into())) {
+                    *sim_state_cb.borrow_mut() = true;
+                }
+            }
+        }
+    };
+
+    let _sim_sub = node.subscribe("/simulator_state", Box::new(sim_cb))?;
+
+
     loop {
         node.spin_once(std::time::Duration::from_millis(1000));
 
-        let state = *state.borrow();
-        let msg = match state {
-            GripperState::Open => State { closed: false, part_sensor: false },
-            GripperState::ClosedWithPart => State { closed: true, part_sensor: true },
-            GripperState::ClosedWithoutPart => State { closed: true, part_sensor: false },
+        let msg = match *state.borrow() {
+            GripperState::Open => State { closed: false, part_sensor: *sim_state.borrow() },
+            GripperState::Closed => State { closed: true, part_sensor: *sim_state.borrow() },
         };
         state_publisher.publish(&msg).unwrap();
     }
